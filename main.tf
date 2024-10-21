f1eb80b23790d268e1b25a6d672371bb15ab57ce
ashraf <239monkeys@gmail.com>
2024-10-21 22:51:26 +0800

    update

--- main.tf
+++ main.tf
@@ -9,3 +9,23 @@ resource "aws_s3_bucket" "bucket-2110" {
   bucket = "bucket-2110"
 
 }
+terraform {
+
+  backend "s3" {
+
+    # Replace this with your bucket name!
+    bucket         = "bucket-2110"
+
+    region         = "eu-west-2"
+  }
+}
+
+resource "aws_instance" "project-2110" {
+
+  ami           = "ami-0903ff18cc3c8e341" # Replace with the desired Amazon Linux 2 AMI for eu-west-2 (bitnami image)
+  instance_type = "t2.micro"  # Change to the desired instance type
+
+  tags = {
+    Name = "project-2110"
+  }
+}
